473852d2a4b7b9840af12bc7c5121ace2649fe9f
phucst123 <huuphucst123@gmail.com>
2023-10-31 17:48:52 +0700

    add dialogs

---
+++ src/lib/views/dialogs/email_dialog.dart
@@ -0,0 +1,55 @@
+import 'package:dugbet/consts/colors.dart';
+import 'package:dugbet/views/widgets/string_button.dart';
+import 'package:flutter/material.dart';
+
+class EmailDialog extends StatelessWidget {
+  const EmailDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Center(child: Text("Email Sent")),
+      titleTextStyle: const TextStyle(
+        color: ColorPalette.defaultText,
+        fontSize: 24,
+        fontFamily: 'Roboto',
+        fontWeight: FontWeight.w700,
+      ),
+      content:
+          const Text("We’ve sent password reset instructions to your mail."),
+      contentTextStyle: const TextStyle(
+        color: ColorPalette.defaultText,
+        fontSize: 16,
+        fontFamily: 'Roboto',
+        fontWeight: FontWeight.w400,
+      ),
+      actions: <Widget>[
+        Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              StringButton(
+                title: "Back to Sign-in",
+                isGradient: true,
+                callback: () {
+                  //Navigator.of(context).pop();
+                },
+              ),
+              StringButton(
+                title: "Resend the email more",
+                style: const TextStyle(
+                  color: ColorPalette.defaultText,
+                  fontSize: 12,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                ),
+                callback: () {},
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ src/lib/views/dialogs/sign_up_dialog.dart
@@ -0,0 +1,44 @@
+import 'package:dugbet/consts/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SignUpDialog extends StatelessWidget {
+  const SignUpDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text("Creating new account",
+                style: TextStyle(
+                  color: ColorPalette.defaultText,
+                  fontSize: 24,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                )),
+            const SizedBox(
+              height: 24,
+            ),
+            const Text("Please wait a moment...",
+                style: TextStyle(
+                  color: ColorPalette.defaultText,
+                  fontSize: 16,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w400,
+                )),
+            const SizedBox(
+              height: 24,
+            ),
+            SvgPicture.asset(
+              "assets/images/demo.svg",
+              fit: BoxFit.cover,
+            ),
+          ]),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+    );
+  }
+}
