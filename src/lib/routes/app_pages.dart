85b7d6154e9431ce2c711f5a7614b1277f6e70fb
Hung Nguyen-Duc <100983291+DucHungGithub@users.noreply.github.com>
2023-12-29 15:30:27 +0700

    agri, momo icon

--- src/lib/routes/app_pages.dart
+++ src/lib/routes/app_pages.dart
@@ -1,6 +1,6 @@
 class AppPage {
   static const splashPage = '/splash';
-  static const homePage = '/home';
+  static const homePage = '/';
   static const settingPage = '/setting';
   static const editProfilePage = '/setting/editprofile';
   static const editNotificationPage = '/setting/editnotification';
@@ -9,7 +9,7 @@ class AppPage {
   static const purchasePage = '/setting/purchase';
   static const transactionHistoryPage = '/transactionHistoryPage';
   static const transactionPage = '/transactionPage';
-  static const chooseCategoryPage = '/';
+  static const chooseCategoryPage = '/chooseCategoryPage';
   static const String loginOpenScreen = '/login_open_screen';
   static const String loginSignUpScreen = '/login_sign_up_screen';
   static const String loginLoginScreen = '/login_login_screen';
--- src/lib/views/widgets/wallet.dart
+++ src/lib/views/widgets/wallet.dart
@@ -1,25 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../consts/color/colors.dart';
 
 class Wallet extends StatelessWidget {
   const Wallet({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
       decoration: BoxDecoration(
-          borderRadius: const BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
-          border: Border.all(
-              color: ColorPalette.white,
-              width: 1
-          )
-
-
-      ),
-
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+          border: Border.all(color: ColorPalette.white, width: 1)),
       child: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -29,7 +23,9 @@ class Wallet extends StatelessWidget {
               children: [
                 SizedBox(width: 15),
                 Text("Total Balance"),
-                SizedBox(width: 20,),
+                SizedBox(
+                  width: 20,
+                ),
                 Icon(Icons.remove_red_eye_outlined)
               ],
             ),
@@ -45,10 +41,15 @@ class Wallet extends StatelessWidget {
                   margin: const EdgeInsets.only(right: 10),
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: ColorPalette.white, width: 2),
                       gradient: const LinearGradient(colors: [
                         ColorPalette.primaryColor,
                         ColorPalette.tertiaryColor
                       ])),
+                  child: Icon(
+                    Icons.attach_money,
+                    color: ColorPalette.white,
+                  ),
                 ),
                 const Text(
                   "234.000d",
@@ -76,42 +77,50 @@ class Wallet extends StatelessWidget {
                   scrollDirection: Axis.horizontal,
                   itemCount: 5,
                   itemBuilder: (context, index) => Row(
-                    children: [
-                      const SizedBox(width: 15),
-                      Container(
-                        width: 314,
-                        height: 80,
-                        margin: const EdgeInsets.only(right: 20),
-                        decoration: BoxDecoration(
-                            color: ColorPalette.white,
-                            borderRadius: BorderRadius.circular(20)
-                        ),
-
-                        child: Center(
-                          child: ListTile(
-                            leading: Container(
-                              height: 60,
-                              width: 60,
-                              decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  gradient: LinearGradient(colors: [
-                                    ColorPalette.primaryColor,
-                                    ColorPalette.tertiaryColor
-                                  ])),
-                            ),
-                            title: const Text("Momo",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
-                            subtitle: const Text("200.000d"),
-                            trailing: const Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Icon(Icons.menu),
-                              ],
+                        children: [
+                          const SizedBox(width: 15),
+                          Container(
+                            width: 314,
+                            height: 200,
+                            margin: const EdgeInsets.only(right: 20),
+                            decoration: BoxDecoration(
+                                color: ColorPalette.white,
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(
+                                    color: ColorPalette.black, width: 1)),
+                            child: Center(
+                              child: ListTile(
+                                leading: Container(
+                                    height: 60,
+                                    width: 60,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                        border: Border.all(
+                                            color: ColorPalette.black,
+                                            width: 1)),
+                                    child:
+                                        Image.asset("assets/images/agri.png",scale: 0.75,)),
+                                title: const Text(
+                                  "Momo",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                subtitle: const Text("200.000d"),
+                                trailing: const Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Icon(
+                                      Icons.menu,
+                                      color: ColorPalette.black,
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                    ],
-                  )),
+                        ],
+                      )),
             ),
             const SizedBox(height: 15)
           ],
