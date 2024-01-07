db51a1ae29bc9e4d6ceda1e5ad2a0804fbcad6e1
phucst123 <huuphucst123@gmail.com>
2024-01-07 15:10:24 +0700

    split controller and view

--- src/lib/controllers/login/signup_controller.dart
+++ src/lib/controllers/login/signup_controller.dart
@@ -1,9 +1,67 @@
-import 'package:get/get.dart';
+import 'package:dugbet/consts/app_export.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignupController extends GetxController {
-  // final _loadingResources = true.obs;
+  final GlobalKey<FormState> signUpFormKey = GlobalKey<FormState>();
 
-  // bool get isLoadingResources => _loadingResources.value;
+  late TextEditingController emailController,
+      userNameController,
+      passwordController;
 
-  
+  var email = "";
+  var username = "";
+  var password = "";
+
+  @override
+  void onInit() async {
+    super.onInit();
+    emailController = TextEditingController();
+    userNameController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
+  @override
+  void onClose() async {
+    emailController.dispose();
+    userNameController.dispose();
+    passwordController.dispose();
+  }
+
+  void signUp() async {
+    print("here1");
+    if (!signUpFormKey.currentState!.validate()) {
+      return;
+    }
+
+    print("here2");
+
+    signUpFormKey.currentState!.save();
+
+    print("email: ${email}");
+    print("pass: ${[password]}");
+
+    if (email != null && password != null) {
+      UserCredential? usercredential;
+      try {
+        usercredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password)
+            .then((value) {
+          Get.snackbar("Success", "Account Created Successfully",
+              snackPosition: SnackPosition.BOTTOM);
+          Get.offAndToNamed(AppPage.loginScreen);
+        });
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          Get.snackbar("Weak Password", "The password provided is too weak.",
+              snackPosition: SnackPosition.BOTTOM);
+        } else if (e.code == 'email-already-in-use') {
+          Get.snackbar("Email Already in Use",
+              "The account already exists for that email.",
+              snackPosition: SnackPosition.BOTTOM);
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
 }
