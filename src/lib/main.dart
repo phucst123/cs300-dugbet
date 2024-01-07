b9d5f7eae985f195c738a0b7084a07e57ee8bb70
tcm03 <21125019@student.hcmus.edu.vn>
2024-01-08 00:27:41 +0700

    Restore main.dart

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -6,36 +6,36 @@ import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-// void main() async{
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Flutter Demo',
-//       // theme: ThemeData(
-//       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//       //   useMaterial3: true,
-//       // ),
-//       getPages: AppRoute.routes(),
-//       initialBinding: AppBinding(),
-//     );
-//   }
-// }
-
-// what is this? => upload data from local DB files to Firestore
-Future<void> main() async {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(GetMaterialApp(
-    home: DataUploaderScreen(),
-  ));
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //   useMaterial3: true,
+      // ),
+      getPages: AppRoute.routes(),
+      initialBinding: AppBinding(),
+    );
+  }
+}
+
+// what is this? => upload data from local DB files to Firestore
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+//   runApp(GetMaterialApp(
+//     home: DataUploaderScreen(),
+//   ));
+// }
