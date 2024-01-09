e3ab35e0b93fd351f52719d6624a48cb2700c4df
Hung Nguyen-Duc <100983291+DucHungGithub@users.noreply.github.com>
2024-01-09 16:56:38 +0700

    change to fnb, walletpage state

--- src/lib/views/pages/transaction/transaction_page.dart
+++ src/lib/views/pages/transaction/transaction_page.dart
@@ -9,6 +9,7 @@ import 'package:intl/intl.dart';
 // ignore: must_be_immutable
 class TransactionPage extends StatelessWidget {
   TransactionPage({super.key});
+
   TransactionController? controller;
 
   @override
@@ -19,185 +20,189 @@ class TransactionPage extends StatelessWidget {
       controller = Get.put<TransactionController>(TransactionController());
     }
     return SafeArea(
-      child: Scaffold(
-        
-        backgroundColor: Colors.white,
-        appBar: NormalHeaderBar(),
-        body: Padding(
-          padding: const EdgeInsets.only(bottom: 16.0, left: 20.0, right: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Obx(
-                () => Column(
-                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "New Transaction",
-                      style: TextThemeBuilder.robotoTextTheme.titleLarge,
-                    ),
-                    DoubleNotch(),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 80,
-                      width: 360,
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey, width: 1),
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8.0, horizontal: 20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Time",
-                              style: TextThemeBuilder
-                                  .robotoTextTheme.headlineLarge,
-                            ),
-                            const SizedBox(
-                              height: 15,
-                            ),
-                            Row(
-                              children: [
-                                InkWell(
-                                  child: Text(
-                                      "${controller!.selectedTime.value.hour}:${controller!.selectedTime.value.minute}",
-                                      style: TextThemeBuilder
-                                          .robotoTextTheme.titleMedium),
-                                  onTap: () {
-                                    controller!.chooseTime();
-                                  },
-                                ),
-                                const SizedBox(
-                                  width: 20,
-                                ),
-                                InkWell(
-                                  child: Text(
-                                      DateFormat("dd:MMM:yyyy")
-                                          .format(
-                                              controller!.selectedDate.value)
-                                          .toString(),
-                                      style: TextThemeBuilder
-                                          .robotoTextTheme.titleMedium),
-                                  onTap: () {
-                                    controller!.chooseDate();
-                                  },
-                                )
-                              ],
-                            )
-                          ],
+      child: SingleChildScrollView(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: NormalHeaderBar(),
+          body: Padding(
+            padding:
+                const EdgeInsets.only(bottom: 16.0, left: 20.0, right: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Obx(
+                  () => Column(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "New Transaction",
+                        style: TextThemeBuilder.robotoTextTheme.titleLarge,
+                      ),
+                      DoubleNotch(),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 80,
+                        width: 360,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey, width: 1),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Time",
+                                style: TextThemeBuilder
+                                    .robotoTextTheme.headlineLarge,
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Row(
+                                children: [
+                                  InkWell(
+                                    child: Text(
+                                        "${controller!.selectedTime.value.hour}:${controller!.selectedTime.value.minute}",
+                                        style: TextThemeBuilder
+                                            .robotoTextTheme.titleMedium),
+                                    onTap: () {
+                                      controller!.chooseTime();
+                                    },
+                                  ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  InkWell(
+                                    child: Text(
+                                        DateFormat("dd:MMM:yyyy")
+                                            .format(
+                                                controller!.selectedDate.value)
+                                            .toString(),
+                                        style: TextThemeBuilder
+                                            .robotoTextTheme.titleMedium),
+                                    onTap: () {
+                                      controller!.chooseDate();
+                                    },
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 80,
-                      width: 360,
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey, width: 1),
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8.0, horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Wallet",
-                                  style: TextThemeBuilder
-                                      .robotoTextTheme.headlineLarge,
-                                ),
-                                const SizedBox(
-                                  height: 15,
-                                ),
-                                Text(
-                                  controller!.selectedWallet.value,
-                                  style: TextThemeBuilder
-                                      .robotoTextTheme.titleMedium,
-                                ),
-                              ],
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                controller!.chooseWallet();
-                              },
-                              child: ClipOval(
-                                child: Center(
-                                  child: SvgPicture.asset(
-                                      "assets/images/momo.svg"),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 80,
+                        width: 360,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey, width: 1),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Wallet",
+                                    style: TextThemeBuilder
+                                        .robotoTextTheme.headlineLarge,
+                                  ),
+                                  const SizedBox(
+                                    height: 15,
+                                  ),
+                                  Text(
+                                    controller!.selectedWallet.value,
+                                    style: TextThemeBuilder
+                                        .robotoTextTheme.titleMedium,
+                                  ),
+                                ],
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  controller!.chooseWallet();
+                                },
+                                child: ClipOval(
+                                  child: Center(
+                                    child: SvgPicture.asset(
+                                        "assets/images/momo.svg"),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
+
+                      // Container(
+                      //   height: 40,
+                      //   width: MediaQuery.of(context).size.width,
+                      //   decoration: BoxDecoration(
+                      //       border: Border.all(color: Colors.grey, width: 1),
+                      //       borderRadius: BorderRadius.circular(10)),
+                      //   child: Padding(
+                      //     padding: const EdgeInsets.symmetric(horizontal: 20),
+                      //     child: Row(
+                      //       children: [
+                      //         Text(
+                      //           "Show more details",
+                      //           style: TextThemeBuilder.robotoTextTheme.labelLarge,
+                      //         ),
+                      //         const Spacer(),
+                      //         IconButton(
+                      //             onPressed: () {},
+                      //             icon: const Icon(Icons.keyboard_arrow_down))
+                      //       ],
+                      //     ),
+                      //   ),
+                      // ),
+
+                      // Container(
+                      //   height: 44,
+                      //   width: 140,
+                      //   decoration: BoxDecoration(
+                      //       color: LightTheme.primaryColor,
+                      //       borderRadius: BorderRadius.circular(25)
+                      //   ),
+
+                      //   child: Center(child: Text("Save",style: TextThemeBuilder.robotoTextTheme.headlineLarge,)),
+                      // )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    StringButton(
+                      text: "Cancel".tr,
+                      callback: onTapCancel,
+                      buttonStyle: CustomButtonStyles.none,
+                      decoration:
+                          CustomButtonStyles.gradientTealToTealDecoration,
+                      width: 140.h,
                     ),
-
-                    // Container(
-                    //   height: 40,
-                    //   width: MediaQuery.of(context).size.width,
-                    //   decoration: BoxDecoration(
-                    //       border: Border.all(color: Colors.grey, width: 1),
-                    //       borderRadius: BorderRadius.circular(10)),
-                    //   child: Padding(
-                    //     padding: const EdgeInsets.symmetric(horizontal: 20),
-                    //     child: Row(
-                    //       children: [
-                    //         Text(
-                    //           "Show more details",
-                    //           style: TextThemeBuilder.robotoTextTheme.labelLarge,
-                    //         ),
-                    //         const Spacer(),
-                    //         IconButton(
-                    //             onPressed: () {},
-                    //             icon: const Icon(Icons.keyboard_arrow_down))
-                    //       ],
-                    //     ),
-                    //   ),
-                    // ),
-
-                    // Container(
-                    //   height: 44,
-                    //   width: 140,
-                    //   decoration: BoxDecoration(
-                    //       color: LightTheme.primaryColor,
-                    //       borderRadius: BorderRadius.circular(25)
-                    //   ),
-
-                    //   child: Center(child: Text("Save",style: TextThemeBuilder.robotoTextTheme.headlineLarge,)),
-                    // )
+                    StringButton(
+                      text: "Save".tr,
+                      callback: onTapSave,
+                      buttonStyle: CustomButtonStyles.none,
+                      decoration:
+                          CustomButtonStyles.gradientTealToTealDecoration,
+                      width: 140.h,
+                    ),
                   ],
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  StringButton(
-                    text: "Cancel".tr,
-                    callback: onTapCancel,
-                    buttonStyle: CustomButtonStyles.none,
-                    decoration: CustomButtonStyles.gradientTealToTealDecoration,
-                    width: 140.h,
-                  ),
-                  StringButton(
-                    text: "Save".tr,
-                    callback: onTapSave,
-                    buttonStyle: CustomButtonStyles.none,
-                    decoration: CustomButtonStyles.gradientTealToTealDecoration,
-                    width: 140.h,
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
